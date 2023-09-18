39c4f0e9e889ca1be98ac8f538ea34bc16eddb4b
william-menezes <williamenezes@outlook.com>
2023-09-18 11:56:31 -0300

    [FEAT] Searchbar js

---
+++ js/searchbar-dropdown.js
@@ -0,0 +1,5 @@
+const searchbarDropdown = document.getElementById("searchbar-dropdown");
+
+function openSearchDrowpdown() {
+  searchbarDropdown.classList.toggle("show");
+}
--- js/searchbar-modal.js
+++ js/searchbar-modal.js
@@ -1,3 +1,11 @@
+const searchbar = document.getElementById("searchbar");
 
+function modalSearchbar() {
+  searchbar.style.display = "block";
+}
 
-
+window.onclick = function (event) {
+  if (event.target == searchbar) {
+    searchbar.style.display = "none";
+  }
+};
