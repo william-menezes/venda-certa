13fb8c34860364bbe739aff984172cc783945d00
William Menezes Damascena <williamenezes@outlook.com>
2023-10-12 17:37:25 -0300

    [FIX] Announcement gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,4 @@
-let slideIndex = 1;
+/* let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -11,7 +11,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("gallery__img");
+  let slides = document.getElementsByClassName("gallery__main-img");
   let dots = document.getElementsByClassName("demo");
 
   if (n > slides.length) {
@@ -26,8 +26,42 @@ function showSlides(n) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < slides.length; i++) {
+    dots[i].className = dots[i].className.replace("active", "");
+  }
+  slides[slideIndex - 1].style.display = "flex";
+  dots[slideIndex - 1].className += " active";
+}
+ */
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].style.display = "flex";
   dots[slideIndex - 1].className += " active";
 }
